b20712b5a1f1fc58d9336c82f36aeea0f40e9593
Toshiaki Asai <qtoship@gmail.com>
2011-07-20 01:13:44 +0900

    first commit

---
+++ copy_tweet_url.rb
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+=begin
+ツイートのURLをコピーするためのmikutterコマンドを追加
+=end
+
+Module.new do
+  Plugin.create(:copy_tweet_url).add_event_filter(:command){ |menu|
+    menu[:smartthread] = {
+      :slug => :smartthread,
+      :name => 'ツイートのURLをコピー',
+      :condition => lambda{ |m| m.message.repliable? },
+      :exec => lambda{ |m| Gtk::Clipboard.copy("http://twitter.com/#!/#{m.message.user.idname}/status/#{m.message[:id]}") },
+      :visible => true,
+      :role => :message }
+    [menu]
+  }
+end
