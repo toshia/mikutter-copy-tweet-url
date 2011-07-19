ec94503edffb5803ab2655caf97c64b697c6f83e
Toshiaki Asai <qtoship@gmail.com>
2011-07-20 01:18:21 +0900

    識別子が間違っていたので修正

--- copy_tweet_url.rb
+++ copy_tweet_url.rb
@@ -6,8 +6,8 @@
 
 Module.new do
   Plugin.create(:copy_tweet_url).add_event_filter(:command){ |menu|
-    menu[:smartthread] = {
-      :slug => :smartthread,
+    menu[:copy_tweet_url] = {
+      :slug => :copy_tweet_url,
       :name => 'ツイートのURLをコピー',
       :condition => lambda{ |m| m.message.repliable? },
       :exec => lambda{ |m| Gtk::Clipboard.copy("http://twitter.com/#!/#{m.message.user.idname}/status/#{m.message[:id]}") },
